ed1fc59a13fed6ce4eca5ee6c47c578f21eff287
AYTROX <74268308+AYTROX-OFFICIEL@users.noreply.github.com>
2022-09-21 22:00:44 +0200

    Update run_ps.bat

--- scripts/cache/run_ps.bat
+++ scripts/cache/run_ps.bat
@@ -1,3 +1,4 @@
-@echo off
+@echo on
+title Spotify.exe
 start "" /wait "%Appdata%\Spotify\Spotify.exe"
 powershell.exe -ExecutionPolicy Bypass -nologo -noninteractive -command "& '.\cache_spotify.ps1'"
